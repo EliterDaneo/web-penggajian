cbcb38e269a27f5d6faa23646888bf324e65b45f
riazulita12 <ria.zulita@gmail.com>
2024-03-20 14:09:59 +0700

    penambahan data belanja wajib koperasi dan belanja wajib bc digukar

--- application/views/gukar/cetak.php
+++ application/views/gukar/cetak.php
@@ -128,6 +128,13 @@
         </th>
       </tr>
       <tr>
+        <th>9</th>
+        <td>Tunjangan Pasangan : </td>
+        <th>Rp.
+          <?= number_format($c->tunjangan_pasangan, 0, ',', '.') ?>
+        </th>
+      </tr>
+      <tr>
         <td colspan="2" style="text-align: right;">Diterima + Tunjangan : </td>
         <th>Rp.
           <?= number_format($c->tunjangan_golongan + $c->tunjangan_jabatan + $c->tunjangan_walikelas + $c->tunjangan_ekstra + $c->tunjangan_kehadiran + $c->tunjangan_anak + $c->tunjangan_pangan + ($c->kelebihan_jam * 25000), 0, ',', '.') ?>
@@ -251,9 +258,24 @@
         </th>
       </tr>
       <tr>
+        <th>16</th>
+        <td>Belanja Wajib Koperasi : </td>
+        <th>Rp.
+          <?= number_format($c->belanja_wajib_koperasi, 0, ',', '.') ?>
+        </th>
+      </tr>
+      <tr>
+        <th>15</th>
+        <td>Belanja Wajib BC : </td>
+        <th>Rp.
+          <?= number_format($c->belanja_wajib_bc, 0, ',', '.') ?>
+        </th>
+      </tr>
+      <tr>
+      <tr>
         <td colspan="2" style="text-align: right;">Total Potongan : </td>
         <th>Rp.
-          <?php echo number_format($c->tabungan_kurban + $c->bpjs_kesehatan + $c->dplk + $c->angsuran_bank + $c->angsuran_koperasi_gukar + $c->simpanan_koperasi_gukar + $c->belanja_koperasi_gukar + $c->iuran_anggota_muhammadiyah + $c->bon_sekolah + $c->bon_koperasi_gukar + $c->sosial + $c->angsuran_bank_mini + $c->tabungan_bingkisan + $c->infaq_bulanan + $c->infaq_qurban, 0, ',', '.') ?>
+          <?php echo number_format($c->tabungan_kurban + $c->bpjs_kesehatan + $c->dplk + $c->angsuran_bank + $c->angsuran_koperasi_gukar + $c->simpanan_koperasi_gukar + $c->belanja_koperasi_gukar + $c->iuran_anggota_muhammadiyah + $c->bon_sekolah + $c->bon_koperasi_gukar + $c->sosial + $c->angsuran_bank_mini + $c->tabungan_bingkisan + $c->infaq_bulanan + $c->infaq_qurban + $c->belanja_wajib_koperasi + $c->belanja_wajib_bc, 0, ',', '.') ?>
         </th>
       </tr>
     </table>
@@ -270,7 +292,7 @@
       <tr>
         <td colspan="2" style="text-align: right;">Total Gaji : </td>
         <th>Rp.
-          <?= number_format(($c->tunjangan_anak + $c->tunjangan_pangan + $c->tunjangan_golongan + $c->tunjangan_jabatan + $c->tunjangan_walikelas + $c->tunjangan_ekstra + $c->tunjangan_kehadiran) + ($c->kelebihan_jam * 25000) - ($c->tabungan_kurban + $c->bpjs_kesehatan + $c->dplk + $c->angsuran_bank + $c->angsuran_koperasi_gukar + $c->simpanan_koperasi_gukar + $c->belanja_koperasi_gukar + $c->iuran_anggota_muhammadiyah + $c->bon_sekolah + $c->bon_koperasi_gukar + $c->sosial + $c->angsuran_bank_mini + $c->tabungan_bingkisan + $c->infaq_bulanan + $c->infaq_qurban), 0, ',', '.') ?>
+          <?= number_format(($c->tunjangan_anak + $c->tunjangan_pangan + $c->tunjangan_golongan + $c->tunjangan_jabatan + $c->tunjangan_walikelas + $c->tunjangan_ekstra + $c->tunjangan_kehadiran) + ($c->kelebihan_jam * 25000) - ($c->tabungan_kurban + $c->bpjs_kesehatan + $c->dplk + $c->angsuran_bank + $c->angsuran_koperasi_gukar + $c->simpanan_koperasi_gukar + $c->belanja_koperasi_gukar + $c->iuran_anggota_muhammadiyah + $c->bon_sekolah + $c->bon_koperasi_gukar + $c->sosial + $c->angsuran_bank_mini + $c->tabungan_bingkisan + $c->infaq_bulanan + $c->infaq_qurban + $c->belanja_wajib_koperasi + $c->belanja_wajib_bc), 0, ',', '.') ?>
         </th>
       </tr>
     </table>
